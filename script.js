e4c8b3d5af12903e06f0223c061092986fe0bc21
alyssaackerman2001 <98808636+alyssaackerman2001@users.noreply.github.com>
2022-05-22 10:16:59 +0630

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const navToggle = document.getElementById("nav-btn");
+const cancelBtn = document.getElementById("cancel-btn");
+const sideNav = document.getElementById("sidenav");
+const modal = document.getElementById("modal");
+
+navToggle.addEventListener("click",function(){
+ sideNav.classList.add("show");
+ modal.classList.add("showModal");
+});
+
+cancelBtn.addEventListener("click",function(){
+ sideNav.classList.remove("show");
+ modal.classList.remove("showmodal");
+});
+
+window.addEventListener("click",function(e){
+ if(e.target === modal) {
+  sideNav.classList.remove("show");
+  modal.classList.remove("showModal");
+ }
+
+});
